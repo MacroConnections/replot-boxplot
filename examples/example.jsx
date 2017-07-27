55fc945127e29ff663f71b3329befdcf0cfc2de6
Matt Farejowicz <mfarejowicz@gmail.com>
2017-07-27 17:13:29 -0400

    boxplot v 0.1 complete

--- examples/example.jsx
+++ examples/example.jsx
@@ -5,10 +5,39 @@ import BoxPlot from "../src/BoxPlot.jsx"
 
 class ExampleApp extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      data: [
+        {gender: "female", score: 93},
+        {gender: "female", score: 95},
+        {gender: "female", score: 60},
+        {gender: "female", score: 74},
+        {gender: "female", score: 82},
+        {gender: "female", score: 90},
+        {gender: "female", score: 71},
+        {gender: "female", score: 81},
+        {gender: "female", score: 88},
+        {gender: "female", score: 85},
+        {gender: "female", score: 94},
+        {gender: "male", score: 96},
+        {gender: "male", score: 64},
+        {gender: "male", score: 74},
+        {gender: "male", score: 77},
+        {gender: "male", score: 76},
+        {gender: "male", score: 82},
+        {gender: "male", score: 81},
+        {gender: "male", score: 88},
+        {gender: "male", score: 84},
+        {gender: "male", score: 91}
+      ]
+    }
+  }
+
   render() {
     return(
       <div>
-        <BoxPlot />
+        <BoxPlot data={this.state.data} groupKey="gender" weightKey="score"/>
       </div>
     )
   }
---
+++ src/Distribution.js
@@ -0,0 +1,51 @@
+
+class Distribution {
+
+  constructor(data, weightKey, groupKey, titleKey) {
+    this.data = []
+    for (let dataPoint of data) {
+      if (groupKey && dataPoint[groupKey] === titleKey){
+        this.data.push(dataPoint[weightKey])
+      }
+      else if (!groupKey) {
+        this.data.push(dataPoint[weightKey])
+      }
+    }
+    this.data = this.data.sort((a,b) => a-b)
+    this.getSummary()
+  }
+
+  getMedian(data){
+    let midIndex = Math.floor(data.length / 2)
+    let isEven = data.length % 2 === 0
+    return isEven ? (data[midIndex] + data[midIndex-1]) / 2 : data[midIndex]
+  }
+
+  getQuartiles(data){
+    let midIndex = Math.floor(data.length / 2)
+    let isEven = data.length % 2 === 0
+    let lowerData = data.slice(0, midIndex)
+    let upperData = data.slice((isEven ? midIndex : midIndex + 1), data.length)
+    return {q1: this.getMedian(lowerData), med: this.getMedian(data), q3: this.getMedian(upperData)}
+  }
+
+  getMean(data){
+    let total = 0
+    for (let i = 0; i < data.length; i++){
+      total += data[i]
+    }
+    return total/data.length
+  }
+
+  getSummary(){
+    this.min = this.data[0]
+    this.max = this.data[this.data.length-1]
+    let quartiles = this.getQuartiles(this.data)
+    this.q1 = quartiles.q1
+    this.median = quartiles.med
+    this.q3 = quartiles.q3
+    this.mean = this.getMean(this.data)
+  }
+}
+
+export default Distribution
