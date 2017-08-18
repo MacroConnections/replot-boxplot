e11036c7159311502a40fdf87e75219012a5bbf1
Matt Farejowicz <mfarejowicz@gmail.com>
2017-08-18 12:38:25 -0400

    new option to draw outliers as points

--- src/BoxPlot.jsx
+++ src/BoxPlot.jsx
@@ -15,17 +15,23 @@ class Plot extends React.Component {
   }
 
   calculatePositions(){
+    let trueMax = this.props.max+this.props.padding
     let positions = {}
-    positions.maxY = ((this.props.max+this.props.padding-this.props.distribution.max)*this.props.unit)
-    positions.q4Y1 = ((this.props.max+this.props.padding-this.props.distribution.max)*this.props.unit)
-    positions.q4Y2 = ((this.props.max+this.props.padding-this.props.distribution.q3)*this.props.unit)
-    positions.rectY = ((this.props.max+this.props.padding-this.props.distribution.q3)*this.props.unit)
-    positions.rectHeight = ((this.props.max+this.props.padding-this.props.distribution.q1)*this.props.unit)-((this.props.max+this.props.padding-this.props.distribution.q3)*this.props.unit)
-    positions.medY = ((this.props.max+this.props.padding-this.props.distribution.median)*this.props.unit)
-    positions.cY = ((this.props.max+this.props.padding-this.props.distribution.mean)*this.props.unit)
-    positions.q0Y1 = ((this.props.max+this.props.padding-this.props.distribution.q1)*this.props.unit)
-    positions.q0Y2 = ((this.props.max+this.props.padding-this.props.distribution.min)*this.props.unit)
-    positions.minY = ((this.props.max+this.props.padding-this.props.distribution.min)*this.props.unit)
+    positions.maxY = ((trueMax-this.props.distribution.max)*this.props.unit)
+    positions.q4Y1 = ((trueMax-this.props.distribution.max)*this.props.unit)
+    positions.q4Y2 = ((trueMax-this.props.distribution.q3)*this.props.unit)
+    positions.rectY = ((trueMax-this.props.distribution.q3)*this.props.unit)
+    positions.rectHeight = ((trueMax-this.props.distribution.q1)*this.props.unit)
+      - ((trueMax-this.props.distribution.q3)*this.props.unit)
+    positions.medY = ((trueMax-this.props.distribution.median)*this.props.unit)
+    positions.cY = ((trueMax-this.props.distribution.mean)*this.props.unit)
+    positions.q0Y1 = ((trueMax-this.props.distribution.q1)*this.props.unit)
+    positions.q0Y2 = ((trueMax-this.props.distribution.min)*this.props.unit)
+    positions.minY = ((trueMax-this.props.distribution.min)*this.props.unit)
+    positions.outliers = []
+    for (let i = 0; i < this.props.distribution.outliers.length; i++) {
+      positions.outliers.push((trueMax-this.props.distribution.outliers[i])*this.props.unit)
+    }
     return positions
   }
 
@@ -73,6 +79,18 @@ class Plot extends React.Component {
       }
     }
 
+    let outliers = []
+    for (let i = 0; i < this.props.distribution.outliers.length; i++) {
+      outliers.push(
+        <circle key={"outlier" + i}
+          cx={this.props.offset} cy={positions.outliers[i]} r={3}
+          fill={this.props.color(this.props.index, this.props.distribution.group)}
+          onMouseOver={this.props.activateTooltip.bind(this, this.props.distribution.data,
+            this.props.distribution.summary, true, this.props.distribution.outliers[i])}
+          onMouseOut={this.props.deactivateTooltip}/>
+      )
+    }
+
     return (
       <Motion
         defaultStyle={initialStyle}
@@ -90,35 +108,40 @@ class Plot extends React.Component {
         }}
       >
         {(interpolatingStyle) =>
-          <g onMouseOver={this.props.activateTooltip.bind(this, this.props.distribution.data, this.props.distribution.summary)}
-            onMouseOut={this.props.deactivateTooltip.bind(this)}>
-            <line x1={-this.props.width/4 + this.props.offset} y1={interpolatingStyle.maxY}
-              x2={this.props.width/4 + this.props.offset} y2={interpolatingStyle.maxY}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              strokeWidth={this.props.style.lineWidth} />
-            <line x1={this.props.offset} y1={interpolatingStyle.q4Y1}
-              x2={this.props.offset} y2={interpolatingStyle.q4Y2}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              strokeWidth={this.props.style.lineWidth}/>
-            <rect x={-this.props.width/2 + this.props.offset} y={interpolatingStyle.rectY}
-              width={this.props.width} height={interpolatingStyle.rectHeight}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              fill={this.props.style.fill} strokeWidth={this.props.style.lineWidth}/>
-            <circle cx={this.props.offset} cy={interpolatingStyle.cY} r={3}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              fill={this.props.style.fill} strokeWidth={this.props.style.lineWidth} />
-            <line x1={-this.props.width/2 + this.props.offset} y1={interpolatingStyle.medY}
-              x2={this.props.width/2 + this.props.offset} y2={interpolatingStyle.medY}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              strokeWidth={this.props.style.lineWidth} />
-            <line x1={this.props.offset} y1={interpolatingStyle.q0Y1}
-              x2={this.props.offset} y2={interpolatingStyle.q0Y2}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              strokeWidth={this.props.style.lineWidth}/>
-            <line x1={-this.props.width/4 + this.props.offset} y1={interpolatingStyle.minY}
-              x2={this.props.width/4 + this.props.offset} y2={interpolatingStyle.minY}
-              stroke={this.props.color(this.props.index, this.props.distribution.group)}
-              strokeWidth={this.props.style.lineWidth} />
+          <g>
+            <g onMouseOver={this.props.activateTooltip.bind(this, this.props.distribution.data, this.props.distribution.summary)}
+              onMouseOut={this.props.deactivateTooltip.bind(this)}>
+              <line x1={-this.props.width/4 + this.props.offset} y1={interpolatingStyle.maxY}
+                x2={this.props.width/4 + this.props.offset} y2={interpolatingStyle.maxY}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                strokeWidth={this.props.style.lineWidth} />
+              <line x1={this.props.offset} y1={interpolatingStyle.q4Y1}
+                x2={this.props.offset} y2={interpolatingStyle.q4Y2}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                strokeWidth={this.props.style.lineWidth}/>
+              <rect x={-this.props.width/2 + this.props.offset} y={interpolatingStyle.rectY}
+                width={this.props.width} height={interpolatingStyle.rectHeight}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                fill={this.props.style.fill} strokeWidth={this.props.style.lineWidth}/>
+              <circle cx={this.props.offset} cy={interpolatingStyle.cY} r={3}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                fill={this.props.style.fill} strokeWidth={this.props.style.lineWidth} />
+              <line x1={-this.props.width/2 + this.props.offset} y1={interpolatingStyle.medY}
+                x2={this.props.width/2 + this.props.offset} y2={interpolatingStyle.medY}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                strokeWidth={this.props.style.lineWidth} />
+              <line x1={this.props.offset} y1={interpolatingStyle.q0Y1}
+                x2={this.props.offset} y2={interpolatingStyle.q0Y2}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                strokeWidth={this.props.style.lineWidth}/>
+              <line x1={-this.props.width/4 + this.props.offset} y1={interpolatingStyle.minY}
+                x2={this.props.width/4 + this.props.offset} y2={interpolatingStyle.minY}
+                stroke={this.props.color(this.props.index, this.props.distribution.group)}
+                strokeWidth={this.props.style.lineWidth} />
+            </g>
+            <g>
+              {outliers}
+            </g>
           </g>
         }
       </Motion>
@@ -174,22 +197,31 @@ class BoxPlot extends React.Component {
     }
   }
 
-  activateTooltip(distribution, summary) {
+  activateTooltip(distribution, summary, outlier, value) {
     let newContents
     if (this.props.tooltipContents){
       newContents = this.props.tooltipContents(distribution, summary)
     }
     else {
-      newContents = (
-        <div>
-          <span>Max: {summary.max}</span><br/>
-          <span>Q3: {summary.q3}</span><br/>
-          <span>Median: {summary.median}</span><br/>
-          <span>Mean: {summary.mean}</span><br/>
-          <span>Q1: {summary.q1}</span><br/>
-          <span>Min: {summary.min}</span><br/>
-        </div>
-      )
+      if (outlier === true) {
+        newContents = (
+          <div>
+            <span>Outlier</span><br/>
+            <span>Value: {value}</span><br/>
+          </div>
+        )
+      } else {
+        newContents = (
+          <div>
+            <span>Max: {summary.max}</span><br/>
+            <span>Q3: {summary.q3}</span><br/>
+            <span>Median: {summary.median}</span><br/>
+            <span>Mean: {summary.mean}</span><br/>
+            <span>Q1: {summary.q1}</span><br/>
+            <span>Min: {summary.min}</span><br/>
+          </div>
+        )
+      }
     }
     this.setState({
       tooltipContents: newContents,
@@ -220,7 +252,7 @@ class BoxPlot extends React.Component {
 
   draw(distributions, min, max, labels){
     let graph
-    let padding = (max-min) / 3
+    let padding = (max-min) / 5
 
     graph = (
       <Axis key="axis" graphTitle={this.props.graphTitle} width={this.props.width}
@@ -233,7 +265,8 @@ class BoxPlot extends React.Component {
         axisStyle={this.props.axisStyle} xAxisMode="discrete" >
         <PlotContainer distributions={distributions} max={max} min={min}
           padding={padding} color={this.colorPlot.bind(this)}
-          style={this.props.graphStyle} initialAnimation={this.props.initialAnimation}
+          style={this.props.graphStyle}
+          initialAnimation={this.props.initialAnimation}
           activateTooltip={this.activateTooltip.bind(this)}
           deactivateTooltip={this.deactivateTooltip.bind(this)}/>
       </Axis>
@@ -248,19 +281,19 @@ class BoxPlot extends React.Component {
     if (this.props.groupKey){
       unique = [...new Set(this.props.data.map(item => item[this.props.groupKey]))]
       for (let group of unique){
-        let d = new Distribution(this.props.data, this.props.weightKey, this.props.groupKey, group)
-        if (!max || d.max > max){
-          max = d.max
+        let d = new Distribution(this.props.data, this.props.weightKey, this.props.drawOutliersAsPoints, this.props.groupKey, group)
+        if (!max || d.data[d.data.length-1] > max){
+          max = d.data[d.data.length-1]
         }
-        if (!min || d.min < min){
-          min = d.min
+        if (!min || d.data[0] < min){
+          min = d.data[0]
         }
         distributions.push(d)
       }
     } else {
-      let d = new Distribution(this.props.data, this.props.weightKey)
-      max = d.max
-      min = d.min
+      let d = new Distribution(this.props.data, this.props.weightKey, this.props.drawOutliersAsPoints)
+      max = d.data[d.data.length-1]
+      min = d.data[0]
       distributions.push(d)
     }
 
@@ -322,7 +355,8 @@ BoxPlot.defaultProps = {
     gridColor: "#DDDDDD",
     lineWidth: 2,
     lineOpacity: 1
-  }
+  },
+  drawOutliersAsPoints: true
 }
 
 BoxPlotResponsive.defaultProps = {
@@ -349,7 +383,8 @@ BoxPlot.propTypes = {
   tooltipColor: PropTypes.string,
   tooltipContents: PropTypes.func,
   graphStyle: PropTypes.object,
-  axisStyle: PropTypes.object
+  axisStyle: PropTypes.object,
+  drawOutliersAsPoints: PropTypes.bool
 }
 
 export default BoxPlotResponsive
--- src/Distribution.js
+++ src/Distribution.js
@@ -1,8 +1,9 @@
 
 class Distribution {
 
-  constructor(data, weightKey, groupKey, group) {
+  constructor(data, weightKey, drawOutliersAsPoints, groupKey, group) {
     this.data = []
+    this.outliers = []
     this.group = group
     for (let dataPoint of data) {
       if (groupKey && dataPoint[groupKey] === group){
@@ -13,7 +14,7 @@ class Distribution {
       }
     }
     this.data = this.data.sort((a,b) => a-b)
-    this.getSummary()
+    this.getSummary(drawOutliersAsPoints)
   }
 
   getMedian(data){
@@ -38,13 +39,38 @@ class Distribution {
     return total/data.length
   }
 
-  getSummary(){
-    this.min = this.data[0]
-    this.max = this.data[this.data.length-1]
+  getSummary(drawOutliersAsPoints){
     let quartiles = this.getQuartiles(this.data)
     this.q1 = quartiles.q1
     this.median = quartiles.med
     this.q3 = quartiles.q3
+
+    if (drawOutliersAsPoints) {
+      let iqr = this.q3 - this.q1
+      let threshold = 1.5 * iqr
+
+      for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i] < this.q1 - threshold) {
+          this.outliers.push(this.data[i])
+        } else {
+          this.min = this.data[i]
+          break
+        }
+      }
+
+      for (let j = this.data.length-1; j >= 0; j--) {
+        if (this.data[j] > this.q3 + threshold) {
+          this.outliers.push(this.data[j])
+        } else {
+          this.max = this.data[j]
+          break
+        }
+      }
+    } else {
+      this.min = this.data[0]
+      this.max = this.data[this.data.length-1]
+    }
+
     this.mean = this.getMean(this.data)
     this.summary = {
       max: this.max,
@@ -52,7 +78,8 @@ class Distribution {
       median: this.median,
       mean: this.mean,
       q1: this.q1,
-      min: this.min
+      min: this.min,
+      outliers: this.outliers
     }
   }
 }
