968b42f74e9d17f7a47444193bf815810ce1752a
askyel <ariel.s.levy1@gmail.com>
2017-09-21 01:27:13 -0400

    transparent boxes, better default example

--- examples/example.jsx
+++ examples/example.jsx
@@ -9,27 +9,27 @@ class ExampleApp extends React.Component {
     super()
     this.state = {
       data: [
-        {gender: "female", score: 93},
-        {gender: "female", score: 95},
-        {gender: "female", score: 60},
-        {gender: "female", score: 74},
-        {gender: "female", score: 82},
-        {gender: "female", score: 90},
-        {gender: "female", score: 71},
-        {gender: "female", score: 81},
-        {gender: "female", score: 88},
-        {gender: "female", score: 85},
-        {gender: "female", score: 12},
-        {gender: "male", score: 96},
-        {gender: "male", score: 66},
-        {gender: "male", score: 74},
-        {gender: "male", score: 77},
-        {gender: "male", score: 76},
-        {gender: "male", score: 82},
-        {gender: "male", score: 81},
-        {gender: "male", score: 88},
-        {gender: "male", score: 84},
-        {gender: "male", score: 91}
+        {age: "19", gender: "female", height: 63},
+        {age: "19", gender: "female", height: 65},
+        {age: "19", gender: "female", height: 60},
+        {age: "19", gender: "female", height: 64},
+        {age: "19", gender: "female", height: 72},
+        {age: "19", gender: "female", height: 70},
+        {age: "19", gender: "female", height: 71},
+        {age: "19", gender: "female", height: 61},
+        {age: "19", gender: "female", height: 68},
+        {age: "19", gender: "female", height: 65},
+        {age: "19", gender: "female", height: 62},
+        {age: "19", gender: "male", height: 76},
+        {age: "19", gender: "male", height: 66},
+        {age: "19", gender: "male", height: 74},
+        {age: "19", gender: "male", height: 67},
+        {age: "19", gender: "male", height: 76},
+        {age: "19", gender: "male", height: 72},
+        {age: "19", gender: "male", height: 71},
+        {age: "19", gender: "male", height: 68},
+        {age: "19", gender: "male", height: 74},
+        {age: "19", gender: "male", height: 71}
       ]
     }
   }
@@ -46,11 +46,11 @@ class ExampleApp extends React.Component {
 
     return(
       <div style={{width: "70%"}}>
-        <BoxPlot data={this.state.data} initialAnimation={false} weightKey="score" />
-        <BoxPlot data={this.state.data} groupKey="gender" weightKey="score" />
-        <BoxPlot data={this.state.data} color={this.colorMe} groupKey="gender" weightKey="score"/>
-        <BoxPlot data={this.state.data} graphTitle="A boxplot!" xTitle="An x axis!" yTitle="A y axis!" color={this.colorMe} groupKey="gender" weightKey="score"/>
-        <BoxPlot data={this.state.data} tooltip graphTitle="A boxplot!" xTitle="An x axis!" yTitle="A y axis!" color={this.colorMe} groupKey="gender" weightKey="score"/>
+        <BoxPlot data={this.state.data} initialAnimation={false} groupKey="age" weightKey="height" xTitle="Age" yTitle="Height (in.)" />
+        <BoxPlot data={this.state.data} groupKey="gender" weightKey="height" xTitle="Gender" yTitle="Height (in.)" />
+        <BoxPlot data={this.state.data} color={this.colorMe} groupKey="gender" weightKey="height" xTitle="Gender" yTitle="Height (in.)" />
+        <BoxPlot data={this.state.data} graphTitle="A boxplot!" xTitle="Gender" yTitle="Height (in.)" color={this.colorMe} groupKey="gender" weightKey="height"/>
+        <BoxPlot data={this.state.data} tooltip graphTitle="A boxplot!" xTitle="Gender" yTitle="Height (in.)" color={this.colorMe} groupKey="gender" weightKey="height"/>
       </div>
     )
   }
